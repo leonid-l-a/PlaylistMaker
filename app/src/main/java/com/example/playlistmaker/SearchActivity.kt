7ba70bc2b5ca4003568510d4fe35c0a9214cba76
leonid-l-a <leo.lapaeff@gmail.com>
2024-11-23 11:21:44 +0100

    commit with 9th sprint changes

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,81 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var inputEditText: EditText
+    private lateinit var clearIcon: ImageView
+
+    companion object {
+        private const val SEARCH_TEXT_KEY = "SEARCH_TEXT_KEY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activiry_search) // Создайте разметку activity_search.xml
+        setContentView(R.layout.activiry_search)
+
+        val backButton: MaterialToolbar = findViewById(R.id.toolbar)
+        backButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        inputEditText = findViewById(R.id.searchEditText)
+        clearIcon = findViewById(R.id.clearIcon)
+
+        clearIcon.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                clearIcon.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val inputText = inputEditText.text.toString()
+                if (inputText.isNotEmpty()) {
+                    Toast.makeText(
+                        this, "Вы ввели текст для поиска: $inputText", Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(this, "Поле поиска пустое", Toast.LENGTH_SHORT).show()
+                }
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val currentText = inputEditText.text.toString()
+        outState.putString(SEARCH_TEXT_KEY, currentText) // Сохранение текста
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoredText = savedInstanceState.getString(SEARCH_TEXT_KEY) // Восстановление текста
+        inputEditText.setText(restoredText)
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,17 +2,56 @@ package com.example.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.MaterialToolbar
 import androidx.appcompat.app.AppCompatActivity
-
+import android.net.Uri
+import android.widget.TextView
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton: Toolbar = findViewById(R.id.backButton)
+        val backButton: MaterialToolbar = findViewById(R.id.backButton)
         backButton.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
+        val shareButton: TextView = findViewById(R.id.share_button)
+        shareButton.setOnClickListener {
+            shareApp()
+        }
+
+        val supportButton: TextView = findViewById(R.id.support_button)
+        supportButton.setOnClickListener {
+            sendSupportEmail()
+        }
+
+        val userAgreementButton: TextView = findViewById(R.id.user_agreement)
+        userAgreementButton.setOnClickListener {
+            openUserAgreement()
+        }
+    }
+
+    private fun shareApp() {
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, "Изучайте Android-разработку с Яндекс Практикумом: https://practicum.yandex.ru/android-developer/")
+        }
+        startActivity(Intent.createChooser(shareIntent, "Поделиться приложением"))
+    }
+
+    private fun sendSupportEmail() {
+        val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:") // Только email-клиенты
+            putExtra(Intent.EXTRA_EMAIL, arrayOf("leo.lapaeff@gmail.com"))
+            putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+        }
+        startActivity(Intent.createChooser(emailIntent, "Написать в поддержку"))
+    }
+
+    private fun openUserAgreement() {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+        startActivity(browserIntent)
     }
 }
