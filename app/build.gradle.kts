c489ad1011c317fee5d8f945252a6bc10a9bd7e0
leonid-l-a <leo.lapaeff@gmail.com>
2025-03-10 18:07:17 +0100

    16th sprint app gradle commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
